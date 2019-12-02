76f4608b67089601c6eeb31766c146238b934305
JohnTendis <ab_surd@list.ru>
2019-12-02 18:28:26 +0300

    Update artii_api.rb

--- artii_api.rb
+++ artii_api.rb
@@ -3,37 +3,13 @@ require 'artii'
 
 VERSION = '0.1.2'.freeze
 
-class MakeAsciiArt < Goliath::API
-  use Goliath::Rack::Params
-
-  def response(env)
-    options = params['font'] ? {:font => params['font'].to_s} : {}
-
-    begin
-      [200, {'Content-Type' => 'text/plain'}, Artii::Base.new(options).asciify(params['text'].to_s)]
-    rescue
-      [500, {'Content-Type' => 'text/plain'}, 'An error has occurred while asciifying.']
-    end
-  end
-end
-
-class FontsList < Goliath::API
-  def response(env)
-    [200, {'Content-Type' => 'text/plain'}, Artii::Base.new.all_fonts.keys.join("\n")]
-  end
-end
-
 class ArtiiAPI < Goliath::API
-  # Make ASCII art
-  gets '/make', MakeAsciiArt
-  # List of available fonts
-  gets '/fonts_list', FontsList
-
   # Render static files from ./public
   use(Rack::Static,
     :root  => Goliath::Application.app_path('public'),
     :urls  => ['/favicon.ico', '/stylesheets'],
     :index => '/index.html')
+  use Goliath::Rack::Params
 
   not_found do
     run Proc.new { |env| [404, {'Content-Type' => 'text/plain'}, 'Not found'] }
@@ -41,6 +17,17 @@ class ArtiiAPI < Goliath::API
 
   # You must use either maps or response, but never both!
   def response(env)
-    raise "#response is ignored when using maps, so this exception won't raise. See spec/integration/rack_routes_spec."
+    case env[ 'PATH_INFO' ]
+      when '/make'
+        options = params['font'] ? {:font => params['font'].to_s} : {}
+        begin
+          [200, {'Content-Type' => 'text/plain'}, Artii::Base.new(options).asciify(params['text'].to_s)]
+        rescue
+          [500, {'Content-Type' => 'text/plain'}, 'An error has occurred while asciifying.']
+        end
+      when '/fonts_list'
+        [200, {'Content-Type' => 'text/plain'}, Artii::Base.new.all_fonts.keys.join("\n")]
+    end
+    #raise "#response is ignored when using maps, so this exception won't raise. See spec/integration/rack_routes_spec."
   end
 end
